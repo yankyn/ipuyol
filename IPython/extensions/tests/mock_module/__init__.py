89d534c3ffe6a3b3d8846b19f39bf3f3a1cb73e5
yanky <yankyn@gmail.com>
2014-04-27 21:43:05 +0300

    Added Interface for the actual completer. Added "System Test" for the line parser. Some name refactoring.

--- IPython/extensions/tests/mock_module/__init__.py
+++ IPython/extensions/tests/mock_module/__init__.py
@@ -2,13 +2,16 @@ __author__ = 'USER'
 
 RETURN_VALUE = 15
 
+
 class BaseMock(object):
     @classmethod
-    def get(cls):
+    def get(cls, **kwargs):
         return RETURN_VALUE
+
 
 class NotBaseMock(object):
     pass
+
 
 class BaseMockSup(BaseMock):
     pass
